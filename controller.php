886d117f0f56f023bc9799d30ae2856e23ddd0d0
drose379 <dylanrose60@DylanRosMacBook.home>
2014-11-19 14:17:33 -0500

    not working

--- controller.php
+++ controller.php
@@ -3,16 +3,6 @@
 require 'insertClass.php';
 $insert = new insertClass;
 
-$array = array(
-	
-		);
-
 $dbc = new PDO('mysql:host=localhost;dbname=learnPDO','root','root');
 
-try {
-	$insert->insertPerson($dbc,$array);
-}
-catch (PDOException $e) {
-	$e->getMessage();
-}
-
+$insert->insertPerson($dbc);
--- insertClass.php
+++ insertClass.php
@@ -2,8 +2,10 @@
 
 class insertClass extends PDO {
 
-public function insertPerson($connection,$infoArray) {
-	
+public function insertPerson($connection) {
+	$query = $connection->prepare("insert into person (id,name,phone) values ('1','Dylan','5542236549')");
+	$query->execute();
+	echo "Inserted";
 }
 
 
