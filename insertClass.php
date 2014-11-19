629e799f945ca8e39d7200e7e9215603167ba646
drose379 <dylanrose60@DylanRosMacBook.home>
2014-11-19 14:30:05 -0500

    not working

--- insertClass.php
+++ insertClass.php
@@ -4,8 +4,7 @@ class insertClass {
 
 public function insertPerson($connection) {
 	$query = $connection->prepare("insert into person (id,name,phone) values ('1','Dylan','5542236549')");
-	$query->execute();
-	echo "Inserted";
+	return $query->execute();
 }
 
 
